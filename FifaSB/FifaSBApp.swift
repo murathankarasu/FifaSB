2c41b3d5ab229930a269c2fc87d471e6b70eba38
murathankarasu <murathankarasu@icloud.com>
2025-02-12 21:15:54 +0300

    Initial Commit

---
+++ FifaSB/FifaSBApp.swift
@@ -0,0 +1,17 @@
+//
+//  FifaSBApp.swift
+//  FifaSB
+//
+//  Created by Murathan Karasu on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FifaSBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
